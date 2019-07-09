c7061f0223d9f5e540ccf92baba3b6c7c66dc9fd
enrico76 <enricodip@gmail.com>
2019-07-09 23:56:59 +0200

    funzione generatrice numeri random + ciclo generatore 16 numeri

--- main.js
+++ main.js
@@ -6,3 +6,23 @@
 // raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio,
 // cioè il numero di volte che l’utente ha inserito un numero consentito.
+console.log("inizio script");
+
+
+function numGen () {
+  var n = Math.floor((Math.random() * 100) +1);
+  return n;
+}
+
+var list =[];
+console.log(list);
+var i =1;
+while (i <= 16) {
+  var num = numGen();
+  controln = list.indexOf(num);
+  if (controln < 0) {
+    list.push(num);
+    i++;
+  }
+
+}
